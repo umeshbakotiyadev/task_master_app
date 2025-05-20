129d27cace853d82d5d1dd37d121b9f4aa49124f
Umesh Dev <umeshbakotiya.dev@gmail.com>
2025-05-20 21:00:38 +0530

    fonts added

---
+++ android/app/src/main/assets/custom/index.ts
@@ -0,0 +1,22 @@
+const FONT = {
+    "BLACK": "BrandonGrotesque-Black",
+    "BLACK_ITALIC": "BrandonGrotesque-BlackItalic",
+    "BOLD": "BrandonGrotesque-Bold",
+    "BOLD_ITALIC": "BrandonGrotesque-BoldItalic",
+    // "EXTRA_BOLD": "BrandonGrotesque-ExtraBold",
+    // "EXTRA_BOLD_ITALIC": "BrandonGrotesque-ExtraBoldItalic",
+    // "EXTRA_LIGHT": "BrandonGrotesque-ExtraLight",
+    // "EXTRA_LIGHT_ITALIC": "BrandonGrotesque-ExtraLightItalic",
+    "ITALIC": "BrandonGrotesque-Italic",
+    "LIGHT": "BrandonGrotesque-Light",
+    "LIGHT_ITALIC": "BrandonGrotesque-LightItalic",
+    "MEDIUM": "BrandonGrotesque-Medium",
+    "MEDIUM_ITALIC": "BrandonGrotesque-MediumItalic",
+    "REGULAR": "BrandonGrotesque-Regular",
+    // "SEMI_BOLD": "BrandonGrotesque-SemiBold",
+    // "SEMI_BOLD_ITALIC": "BrandonGrotesque-SemiBoldItalic",
+    "THIN": "BrandonGrotesque-Thin",
+    "THIN_ITALIC": "BrandonGrotesque-ThinItalic"
+};
+
+export { FONT }
