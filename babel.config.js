6ec39618be9d37c4a758c896c9b0070100b51ca6
Umesh Dev <umeshbakotiya.dev@gmail.com>
2025-05-20 14:51:39 +0530

    stage-1

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  "plugins": [
+    'react-native-reanimated/plugin',
+    ['module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null, // DEPRECATED
+        whitelist: null, // DEPRECATED
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    ["module-resolver", {
+      "root": ['./src'],
+      "extensions": [".tsx", ".ts", ".js", ".jsx", ".json", ".jsonc"],
+      "alias": { "*": "./src/", }
+    }
+    ]
+  ]
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,22 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const defaultAssetExts = require('metro-config/src/defaults/defaults').assetExts;
+const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
-
+const config = {
+    'transformer': {
+        'babelTransformerPath': require.resolve("react-native-svg-transformer")
+    },
+    'resolver': {
+        'assetExts': defaultAssetExts.filter(ext => ext !== "svg"),
+        'sourceExts': [...defaultSourceExts, "svg"]
+    }
+};
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
